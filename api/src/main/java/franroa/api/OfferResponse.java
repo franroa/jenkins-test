c8d99424015782be9e0faac074d217ade8ba91f1
Francisco Roa Prieto <F.Prieto@sofort.com>
2019-02-12 13:56:37 +0100

    Add @JsonProperty annotation for consistency with the request object

--- api/src/main/java/franroa/api/OfferResponse.java
+++ api/src/main/java/franroa/api/OfferResponse.java
@@ -1,9 +1,20 @@
 package franroa.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class OfferResponse {
+    @JsonProperty("id")
     public Long id;
+
+    @JsonProperty("name")
     public String name;
+
+    @JsonProperty("price")
     public Long price;
+
+    @JsonProperty("currency")
     public String currency;
+
+    @JsonProperty("expires_at")
     public String expires_at;
 }
